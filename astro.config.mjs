5cbfd4e8a39770dfbdf9901ea19c8799bbf9f45c
uncoded-apps <104200486+uncoded-apps@users.noreply.github.com>
2025-10-26 14:07:31 +0000

    Added navigation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
-import tailwindcss from "@tailwindcss/vite";
 
-// https://astro.build/config
-export default defineConfig({  
-    vite: {
-    plugins: [tailwindcss()],
-  },
+import tailwindcss from '@tailwindcss/vite';
+
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
