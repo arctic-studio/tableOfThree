d9e7ffd51909cfce8a9ca869ec3defb6973e717b
arctic-studio <jp@arctic.studio>
2025-12-02 16:13:35 +0000

    added cloudflare adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from 'astro/config';
+import cloudflare from '@astrojs/cloudflare';
 
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+  adapter: cloudflare(),
+  output: 'server',
 });
